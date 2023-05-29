0aaad5d170d53d065a9dd62d85a9535f4438aee7
Alexey Shevchenko <o.shevchenko@relay42.com>
2023-05-29 10:01:26 +0200

    v2

---
+++ js/contacts-button-widget-v2.js
@@ -0,0 +1,28 @@
+/*********** [START] Contact widget button ***********/
+window.contactWidgetInit = function() {
+	$('#contact_widget_button').on('click', function() {
+		$(this).toggleClass('clicked');
+		$('.contact-widget-dropdown').toggleClass('expanded');
+		$('.contact-widget-icon .fa-envelope, #back-top').removeClass('d-none');
+		$('.contact-widget-icon .chat-icon, #back-top').toggleClass('d-none');
+		$('.contact-widget-icon .contact-widget-text').toggleClass('d-none');
+		$('#contact_widget_substrate').toggleClass('active');
+	});
+
+	$('#contact_widget_substrate').on('click', function() {
+		$(this).removeClass('active');
+		$('.contact-widget-dropdown').removeClass('expanded');
+		$('.contact-widget-icon .fa-envelope, #back-top').removeClass('d-none');
+		$('.contact-widget-icon .chat-icon, #back-top').removeClass('d-none');
+		$('.contact-widget-icon .contact-widget-text').toggleClass('d-none');
+		$('#contact_widget_button').removeClass('clicked');
+	});
+
+	$('.contact-widget-dropdown').click(function(e) {
+		e.stopPropagation();
+	});
+};
+$(document).ready(function() {
+	window.contactWidgetInit();
+});
+/*********** [END] Contact widget button ***********/
